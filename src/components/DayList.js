ba0f3092dbea7ec53bed89c43d4db91f5bc1bfc8
DivyaJagadish <divyajagadish21@gmail.com>
2021-04-13 00:08:08 -0700

    Completes DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,19 @@
 import React from "react";
 import classNames  from "classnames";
-
+import  DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
+
+  const List = props.days.map((day,index) =>
+  <DayListItem 
+  key= {index}
+  name={day.name} 
+  spots={day.spots} 
+  selected ={day.name === props.day}
+  setDay={props.setDay}  />
+  )
   
   return (
-    <ul></ul>
+    <ul>{List}</ul>
   );
 }
