c7ab5a99484550788c435bd8fb11cfbc13192adc
DivyaJagadish <divyajagadish21@gmail.com>
2021-04-13 00:33:00 -0700

    adds usestate to Daylist

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 const days = [
@@ -21,6 +20,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [Defaultday,setDay] = useState("Monday")
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,8 +33,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
   <DayList
   days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
+  day= {Defaultday}
+  setDay={setDay}
 />
 </nav>
 <img
