518d1c57a0f45681e14511713c13c1e196a3bac9
DivyaJagadish <divyajagadish21@gmail.com>
2021-04-16 14:55:08 -0700

    using custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
 
-export  default function useVisualMode(Intial){
-const  [mode,setMode] =useState(Intial);
+export  default function useVisualMode(initial){
+const  [mode,setMode] =useState(initial);
 const [history, setHistory] = useState([initial]);
 
-function transition(newMode){
+function transition(newMode,replace = false){
   setMode(prev=> prev = newMode);
-  setHistory(prev=> prev = newMode);
+  if (replace){
+  history[history.length-1] = newMode;
+}
+  else{
+  setHistory(prev=> prev = [...prev ,newMode]);
+  }
 };
 
-function back() { /* ... */ }
+function back() { 
+if(history.length> 1) {
+history.pop();
+const newMode = history[history.length - 1]
+setMode(prev => prev = newMode)
+}
+}
  return {mode, transition,back }
 }
